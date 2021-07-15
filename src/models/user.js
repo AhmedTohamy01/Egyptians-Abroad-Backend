c7057b2afac43de2c1d73ef5edbe943d0c26f957
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-07-15 22:39:32 +0200

    feat: update User model with a new custom function to validate user by credentials

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -41,6 +42,34 @@ userSchema.virtual('comments', {
 	localField: '_id',
 	foreignField: 'owner'
 })
+
+// hash user's password before saving to database
+userSchema.pre('save', async function (next) {
+  const user = this
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+
+  next()
+})
+
+// add custom function to valdiate user by credentials 
+userSchema.statics.findByCredentials = async (email, password) => {
+	const user = await User.findOne({ email })
+
+	if (!user) {
+		throw new Error('This user is NOT registered!')
+	}
+
+	const isPasswordMatch = bcrypt.compare(password, user.password)
+
+	if (!isPasswordMatch) {
+		throw new Error('The password is NOT correct!')
+  }
+
+	return user
+}
 
 const User = mongoose.model('User', userSchema)
 
