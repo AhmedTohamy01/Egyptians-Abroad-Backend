9911136984c9c60b4e9f611fc9953bbbd4323fa4
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-07-16 21:30:10 +0200

    feat: modify signup/login endpoints to add token | finalize get-my-user endpoint

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+const auth = require('../middleware/auth')
+
+
+// get all users (temp for testing)
+router.get('/users', async (req, res) => {
+  const users = await User.find({})
+  res.send(users)
+})
+
 
 // signup a user account
 router.post('/users/signup', async (req, res) => {
   const user = new User(req.body)
 
   try {
+		const token = await user.generateAuthToken()
     await user.save()
-    res.status(201).send({user})
+    res.status(201).send(user)
   } catch (e) {
     res.status(400).send(e)
   }
@@ -19,16 +29,19 @@ router.post('/users/login', async (req, res) => {
 
   try {
 		const user = await User.findByCredentials(req.body.email, req.body.password)
-		res.status(200).send({user})
+		const token = await user.generateAuthToken()
+		res.status(200).send(user)
 	} catch(e) {
 		res.status(400).send(e)
 	}
 })
 
 // get my user
-router.patch('/users/me', (req, res) => {
-  res.send('This is update my user endpoint')
+router.get('/users/me', auth, (req, res) => {
+  res.send(req.user)
 })
+
+
 
 // update my user
 router.patch('/users/me', (req, res) => {
