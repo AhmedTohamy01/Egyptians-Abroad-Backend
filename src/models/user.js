6d9221730d2f3f9e39937cdfedf04ee8164a3555
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-07-16 21:27:07 +0200

    feat: add tokens field & add generateAuthToken function in User Model

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -26,21 +27,28 @@ const userSchema = new mongoose.Schema({
     trim: true,
     minlength: 7,
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 })
 
-
-// add virtual posts field 
+// add virtual posts field
 userSchema.virtual('posts', {
-	ref: 'Post',
-	localField: '_id',
-	foreignField: 'owner'
+  ref: 'Post',
+  localField: '_id',
+  foreignField: 'owner',
 })
 
-// add virtual comments field 
+// add virtual comments field
 userSchema.virtual('comments', {
-	ref: 'Comment',
-	localField: '_id',
-	foreignField: 'owner'
+  ref: 'Comment',
+  localField: '_id',
+  foreignField: 'owner',
 })
 
 // hash user's password before saving to database
@@ -54,21 +62,33 @@ userSchema.pre('save', async function (next) {
   next()
 })
 
-// add custom function to valdiate user by credentials 
+// add custom function to valdiate user by credentials
 userSchema.statics.findByCredentials = async (email, password) => {
-	const user = await User.findOne({ email })
+  const user = await User.findOne({ email })
 
-	if (!user) {
-		throw new Error('This user is NOT registered!')
-	}
-
-	const isPasswordMatch = bcrypt.compare(password, user.password)
-
-	if (!isPasswordMatch) {
-		throw new Error('The password is NOT correct!')
+  if (!user) {
+    throw new Error('This user is NOT registered!')
   }
 
-	return user
+  const isPasswordMatch = bcrypt.compare(password, user.password)
+
+  if (!isPasswordMatch) {
+    throw new Error('The password is NOT correct!')
+  }
+
+  return user
+}
+
+// Generate Authentication Token
+userSchema.methods.generateAuthToken = async function () {
+  const user = this
+  const token = await jwt.sign(
+    { _id: user._id.toString() },
+    process.env.JWT_SECRET
+  )
+  user.tokens = user.tokens.concat({ token: token })
+  await user.save()
+	
 }
 
 const User = mongoose.model('User', userSchema)
