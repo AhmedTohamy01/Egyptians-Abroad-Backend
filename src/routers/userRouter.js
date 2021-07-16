31335635fa3b03ca5d24fdc64266f159eda3b2b9
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-07-16 22:52:17 +0200

    feat: finalize logout-from-one-device endpoint

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,20 +3,18 @@ const router = express.Router()
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 
-
 // get all users (temp for testing)
 router.get('/users', async (req, res) => {
   const users = await User.find({})
   res.send(users)
 })
 
-
 // signup a user account
 router.post('/users/signup', async (req, res) => {
   const user = new User(req.body)
 
   try {
-		const token = await user.generateAuthToken()
+    const token = await user.generateAuthToken()
     await user.save()
     res.status(201).send(user)
   } catch (e) {
@@ -26,14 +24,13 @@ router.post('/users/signup', async (req, res) => {
 
 // login with a user account
 router.post('/users/login', async (req, res) => {
-
   try {
-		const user = await User.findByCredentials(req.body.email, req.body.password)
-		const token = await user.generateAuthToken()
-		res.status(200).send(user)
-	} catch(e) {
-		res.status(400).send(e)
-	}
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    const token = await user.generateAuthToken()
+    res.status(200).send(user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
 // get my user
@@ -43,37 +40,44 @@ router.get('/users/me', auth, (req, res) => {
 
 // update my user
 router.patch('/users/me', auth, async (req, res) => {
-	const updates = Object.keys(req.body)
-	const allowedUpdates = ['name', 'email', 'password']
-	const isValidUpdates = updates.every(update => allowedUpdates.includes(update))
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['name', 'email', 'password']
+  const isValidUpdates = updates.every((update) =>
+    allowedUpdates.includes(update)
+  )
 
-	if (!isValidUpdates) {
-		throw new Error('Invalid Updates!')
+  if (!isValidUpdates) {
+    throw new Error('Invalid Updates!')
   }
 
-	try {
-		updates.forEach(update => req.user[update] = req.body[update])
-		await req.user.save()
-		res.status(200).send(req.user)
-	} catch(e) {
-		res.status(400).send(e)
-	}
-
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]))
+    await req.user.save()
+    res.status(200).send(req.user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
 // delete my user
 router.delete('/users/me', auth, async (req, res) => {
-	try {
-		await req.user.remove()
-		res.status(200).send(req.user)
-	} catch(e) {
-		res.status(400).send(e)
-	}
+  try {
+    await req.user.remove()
+    res.status(200).send(req.user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
-// logout a user
-router.post('/users/logout', (req, res) => {
-  res.send('This is logout endpoint')
+// logout a user from one device
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(item => item.token !== req.token)
+    await req.user.save()
+    res.send(req.user)
+  } catch (e) {
+    res.status(500).send(e)
+  }
 })
 
 // logout a user from all devices
