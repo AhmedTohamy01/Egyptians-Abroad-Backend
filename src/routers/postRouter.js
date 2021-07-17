dc31e5ef4c02e683c0d89d1ff306a7cc753b2791
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-07-17 21:31:44 +0200

    feat: finalize add-new-post endpoint

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const Post = require('../models/post')
+const auth = require('../middleware/auth')
 
-// add a post
-router.post('/posts', (req, res) => {
-  res.send('This is add new post endpoint')
+// add new post
+router.post('/posts/new', auth, async (req, res) => {
+  const post = new Post({
+		...req.body,
+		owner: req.user._id
+	})
+	try {
+		await post.save()
+		res.status(201).send(post)
+	} catch(e) {
+		res.status(400).send(e)
+	}
 })
 
 // get all posts
