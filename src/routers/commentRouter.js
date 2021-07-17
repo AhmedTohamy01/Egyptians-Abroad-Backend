a9e7baf62cbfdbd47d89d884f3d75dd6a344a58c
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-07-18 00:29:04 +0200

    feat: finalize add-new-comment endpoint

--- src/routers/commentRouter.js
+++ src/routers/commentRouter.js
@@ -1,12 +1,25 @@
 const express =require('express')
 const router = express.Router()
+const Comment = require('../models/comment')
+const auth = require('../middleware/auth')
 
 // add a comment
-router.post('/comments', (req, res) => {
-  res.send('This is add new comment endpoint')
+router.post('/comments/:postId/new', auth, async (req, res) => {
+  const comment = new Comment({
+    ...req.body,
+    owner: req.user._id,
+		parent_post: req.params.postId
+  })
+
+	try {
+		await comment.save()
+		res.status(201).send(comment)
+	} catch(e) {
+
+	}
 })
 
-// add my comments
+// get my comments
 router.get('/comments/me', (req, res) => {
   res.send('This is get my comments endpoint')
 })
