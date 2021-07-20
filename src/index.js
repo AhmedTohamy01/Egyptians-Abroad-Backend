01cfc592d436ee74442d63681fd33f433be01fc0
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-07-21 01:58:19 +0200

    feat: use CORS package in express app

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ require('../src/db/mongoose')
 const userRouter = require('../src/routers/userRouter')
 const postRouter = require('../src/routers/postRouter')
 const commentRouter = require('../src/routers/commentRouter')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(postRouter)
