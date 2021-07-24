a508c33ae43098ccb2dac5d757a27d2a23862bbb
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-07-24 03:37:06 +0200

    feat: add get-my-posts endpoint with match, sort, pagination options

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -6,35 +6,72 @@ const auth = require('../middleware/auth')
 // add new post
 router.post('/posts/new', auth, async (req, res) => {
   const post = new Post({
-		...req.body,
-		owner: req.user._id
-	})
-	try {
-		await post.save()
-		res.status(201).send(post)
-	} catch(e) {
-		res.status(400).send(e)
-	}
+    ...req.body,
+    owner: req.user._id,
+  })
+  try {
+    await post.save()
+    res.status(201).send(post)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
 // get all posts
 router.get('/posts', async (req, res) => {
-	try {
-		const posts = await Post.find({})
-		res.status(200).send(posts)
-	} catch (e) {
-		res.status(400).send(e)
-	}
+  try {
+    const posts = await Post.find({})
+    res.status(200).send(posts)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
-// get my posts
+// get my posts without match, sort or pagination options.
+//
+// router.get('/posts/me', auth, async (req, res) => {
+//   try {
+// 		const posts = await Post.find({ owner: req.user._id })
+// 		res.status(200).send(posts)
+// 	} catch(e) {
+// 		res.status(400).send(e)
+// 	}
+// })
+
+// get my posts WITH match, sort or pagination options.
+//
+// GET /posts/me?completed=true
+// GET /posts/me?limit=10&skip=20
+// GET /posts/me?sortBy=createdAt:desc
 router.get('/posts/me', auth, async (req, res) => {
+  const match = {}
+  const sort = {}
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true'
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':')
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+  }
+
   try {
-		const posts = await Post.find({ owner: req.user._id })
-		res.status(200).send(posts)
-	} catch(e) {
-		res.status(400).send(e)
-	}
+    await req.user
+      .populate({
+        path: 'posts',
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort,
+        },
+      })
+      .execPopulate()
+    res.send(req.user.posts)
+  } catch (e) {
+    res.status(500).send()
+  }
 })
 
 module.exports = router
